e44157d511dcca7b6a13777fddaf7067d608f8a5
ademaill <antoine.demaillepro@yahoo.com>
2025-08-05 12:58:56 +0200

    frontend

---
+++ frontend/src/ts/main.ts
@@ -0,0 +1,42 @@
+import "../style.css";
+import "../output.css";
+import { laodDashboard } from "./dashboard";
+
+
+const routes: { [key: string]: string} = {
+    home: 'home.html',
+    login: 'login.html',
+    register: 'register.html',
+}
+
+async function loadLayout() {
+  const layoutResp = await fetch('./src/pages/layout.html');
+  const layoutHtml = await layoutResp.text();
+  document.body.innerHTML = layoutHtml;
+}
+
+async function loadPage(hash: string)
+{
+    const raw = location.hash.slice(1);
+    const page = raw.replace('.html', '') || 'home';
+    const pageRes = await fetch(`./src/pages/${page}.html`);
+    const pageHtml = await pageRes.text();
+
+    const app = document.getElementById('app');
+    if (app) app.innerHTML = pageHtml;
+
+    if (page === 'dashboard') {
+      laodDashboard();
+    }
+}
+
+
+window.addEventListener('hashchange', () => {
+    const hash = location.hash.replace('#', '');
+    loadPage(hash);
+});
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await loadLayout();
+  await loadPage('home');
+});
